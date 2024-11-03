4695f704c8c3f3567fdaa16d18d23dce9311f31c
Jgab88 <jerry.gabrie@gmail.com>
2024-11-03 14:54:12 -0600

    Created New branch for Encoder Publishing

--- build/set_frame_id/prefix_override/sitecustomize.py
+++ build/set_frame_id/prefix_override/sitecustomize.py
@@ -1,3 +1,4 @@
 import sys
-sys.real_prefix = sys.prefix
-sys.prefix = sys.exec_prefix = '/home/jg/bno055_hardware/install/set_frame_id'
+if sys.prefix == '/usr':
+    sys.real_prefix = sys.prefix
+    sys.prefix = sys.exec_prefix = '/home/jg/bno055_hardware/install/set_frame_id'
